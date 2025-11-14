b1e2ab4358f469860ddea3d64bb03d6c8a809f4e
SCOTT MACKAY <2501389@uad.ac.uk>
2025-11-14 11:04:09 +0000

    Improved movement and added camera
    Made the movement function better and implemented a camera that rotates around the player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 50f;
-    public InputAction playerMove;
-
-
+    // Rigidbody of the player.
     private Rigidbody rb;
-    private Vector3 moveDirection;
 
+    // Movement along X and Y axes.
+    private float movementX;
+    private float movementY;
+    private float MouseX;
+    private Vector3 angleVelocity;
 
-    private void OnEnable()
-    {
-        playerMove.Enable();
-    }
+    // Speed at which the player moves.
+    public float speed = 10f;
 
-    private void OnDisable()
-    {
-        playerMove.Disable();
-    }
+    public float sensitivity = 50f;
 
+    // Start is called before the first frame update.
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>(); 
+        Cursor.lockState = CursorLockMode.Locked;
+        // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
-        moveDirection = playerMove.ReadValue<Vector3>();
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+        
+    }
+    private void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X");
+        angleVelocity = new Vector3(0, MouseX * sensitivity, 0);
     }
 
+    // FixedUpdate is called once per fixed frame-rate frame.
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.z * moveSpeed);
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+
+        Quaternion deltaRotation = Quaternion.Euler(angleVelocity * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
